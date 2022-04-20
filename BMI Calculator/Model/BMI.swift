f7f95bbf0560cae06502ad55679e15120469ed56
Nyade <niinyade@icloud.com>
2022-04-20 18:16:46 -0300

    Add a new model file with a struct. Add new functions to model files to change the background color and text; also optional bindings, chaining and coalescing methods. Add a new controller view to handle all func callings to do what is supposed to do.

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Nyade on 20/04/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
